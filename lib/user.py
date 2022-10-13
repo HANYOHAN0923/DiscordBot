cedc671b084953eca38e14f701bfee966914beb3
한요한 <han-yohan@han-yohan-ui-MacBookAir.local>
2022-10-13 19:42:54 +0900

    cleaning CODE

--- lib/user.py
+++ lib/user.py
@@ -9,9 +9,190 @@ default_money = 30000000
 wb = load_workbook("./db/userDB.xlsx")
 ws = wb.active
 
-def signup(_name, _id):
-    ws.cell(row=2, column=c_name, value = _name)
-    ws.cell(row=2, column=c_id, value = _id)
-    ws.cell(row=2, column=c_money, value = default_money)
+def loadFile():
+    wb = load_workbook("./db/userDB.xlsx")
+    ws = wb.active
 
-    wb.save('./db/userDB.xlsx')
+def saveFile():
+    wb.save("./db/userDB.xlsx")
+    wb.close()
+
+def checkUser(_name, _id):
+    print("user.py - checkUser")
+    print(str(_name) + "<" + str(_id) + ">의 존재 여부 확인")
+    print("")
+
+    loadFile()
+
+    userNum = checkUserNum()
+    print("등록된 유저수: ", userNum)
+    print("")
+
+    print("이름과 고유번호 탐색")
+    print("")
+
+    for row in range(2, 3+userNum):
+        print(row, "번째 줄 name: ", ws.cell(row,c_name).value)
+        print("입력된 name: ", _name)
+        print("이름과 일치 여부: ", ws.cell(row, c_name).value == _name)
+
+        print(row,"번째 줄 id: ", ws.cell(row,c_id).value)
+        print("입력된 id: ", hex(_id))
+        print("고유번호정보와 일치 여부: ", ws.cell(row, c_id).value == hex(_id))
+        print("")
+
+        if ws.cell(row, c_name).value == _name and ws.cell(row,c_id).value == hex(_id):
+            print("등록된  이름과 고유번호를 발견")
+            print("등록된  값의 위치: ",  row, "번째 줄")
+            print("")
+
+            saveFile()
+
+            return True, row
+            break
+        else:
+            print("등록된 정보를 탐색 실패, 재탐색 실시")
+
+    saveFile()
+    print("발견 실패")
+
+    return False, None
+
+def checkUserNum():
+    loadFile()
+
+    count = 0
+
+    for row in range(2, ws.max_row+1):
+        if ws.cell(row,c_name).value != None:
+            count = count+1
+        else:
+            count = count
+    return count
+    
+
+def findRow(_name, _id):
+    loadFile()
+
+    print("등록된 유저수: ", checkUserNum())
+    print("")
+
+    print("이름과 고유번호 탐색")
+    for row in range(2, ws.max_row+2):
+        print(row, "번째 줄 name: ", ws.cell(row,c_name).value)
+        print("입력된 name: ", _name)
+        print("이름과 일치 여부: ", ws.cell(row, c_name).value == _name)
+
+        print(row,"번째 줄 id: ", ws.cell(row,c_id).value)
+        print("입력된 id: ", hex(_id))
+        print("고유번호정보와 일치 여부: ", ws.cell(row, c_id).value == hex(_id))
+        print("")
+
+        if ws.cell(row, c_name).value == _name and ws.cell(row,c_id).value == hex(_id):
+            print("등록된  이름과 고유번호를 발견")
+            print("등록된  값의 위치: ",  row, "번째 줄")
+
+            saveFile()
+
+            return row
+            break
+        else:
+            print("등록된 정보를 탐색 실패, 재탐색 실시")
+
+    saveFile()
+
+    return None
+
+def getMoney(_name,_row):
+    print("user.py - getMoney")
+    loadFile()
+
+    print(_name, "의 돈을 탐색")
+
+    result = ws.cell(_row, c_money).value
+    print(_name,"의 보유 자산: ", result)
+
+    saveFile()
+
+    return result
+
+def remit(sender, sender_row, receiver, receiver_row, _amount):
+    print("user.py - remit")
+    #loadFile()
+    
+    print("보내는 사람: ", sender)
+    print("받는 사람: ", receiver)
+    print("보내는 돈: ", _amount)
+    print("")
+
+    modifyMoney(receiver, receiver_row, _amount)
+    modifyMoney(sender, sender_row, -int(_amount))
+
+    print("")
+
+def modifyMoney(_target, _row, _amount):
+    print("user.py - modifyMoney")
+    loadFile()
+
+    print(_target, "의 자산데이터 수정")
+    print(_target, "의 자산: " + str(ws.cell(_row, c_money).value))
+    print("추가할 액수: ", _amount)
+    ws.cell(_row, c_money).value += int(_amount)
+
+    print("자산데이터 수정 완료")
+    print("수정된", _target, "의 자산: ", ws.cell(_row, c_money).value)
+    
+    saveFile()
+
+def checkRow():
+    loadFile()
+    print("첫번쨰 빈 곳을 탐색")
+    for row in range(2, ws.max_row + 1):
+        if ws.cell(row,1).value is None:
+            return row
+            break
+    _result = ws.max_row+1
+    saveFile()
+
+    return _result
+
+def signup(_name, _id):
+    loadFile()
+    print("첫번째 빈곳: ", checkRow())
+    _row = checkRow()
+
+    print("데이터 추가 시작")
+    ws.cell(row=_row, column=c_name, value=_name)
+    print("이름 추가 | ",  ws.cell(_row,c_name).value)
+    ws.cell(row=_row, column=c_id, value =hex(_id))
+    print("고유번호 추가 | ", ws.cell(_row,c_id).value)
+    ws.cell(row=_row, column=c_money, value = default_money)
+    print("기본 자금 지급 | ", ws.cell(_row,c_money).value)
+
+    saveFile()
+
+    print("데이터 추가 완료")
+
+def userInfo(_name, _id):
+    loadFile()
+
+    userRow = findRow(_name, _id)
+
+    if not userRow == None:
+        print("사용자 정보 발견, 보유 자산과 레벨을 반환")
+        print("보유자산: ", ws.cell(userRow,c_money).value)
+        return ws.cell(userRow,c_money).value
+    else:
+        #사용자 정보 없음
+        return None
+
+
+def delete():
+    loadFile()
+
+    print("유저 데이터를 삭제")
+
+    ws.delete_rows(2,ws.max_row)
+    saveFile()
+
+    print("데이터 삭제 완료")
--- main.py
+++ main.py
@@ -5,20 +5,19 @@ from dotenv import load_dotenv
 
 
 # Custom Python Library
-import lib.sb as sb
-import lib.money as money
-import lib.godgame as godgame
-import lib.hairlossbeam as hairlossbeam
-import lib.recommendfood as recommendfood
-import lib.search_lol_userInfo as search_lol_userInfo
-import lib.search_tft_userInfo as search_tft_userInfo
-import lib.recommendfood as recommendfood
-
-import lib.user as user
+from lib.sb import sb
+from lib.money import foreignCurrency
+from lib.godgame import godgame
+from lib.hairlossbeam import hairlossbeam
+from lib.recommendfood import recommendfood
+from lib.search_lol_userInfo import search_lol_userInfo
+from lib.search_tft_userInfo import search_tft_userInfo
+from lib.leavework import countTime
+from lib.user import signup, findRow, userInfo, delete, getMoney, remit, checkUser
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-RIOT_TOKEN = "RGAPI-8a42353b-0e5d-47dc-8730-1de8741c79f9"
+RIOT_TOKEN = os.getenv("RIOT_TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -28,45 +27,164 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print(f'{bot.user} online!')
 
+@bot.command()
+async def 명령어(ctx):
+    embed = discord.Embed(title = "Obunga Bot#1375", description = "열구가 좋아 죽습니다", color = 0x6E17E3) 
+    embed.add_field(name = bot.command_prefix + "명령어", value = "Obunga Bot의 명령어를 확인합니다", inline = False)
+    embed.add_field(name = bot.command_prefix + "탈모빔 유저이름", value = "특정 유저에게 탈모빔을 날립니다. 확률이 존재합니다.", inline = False)
+    embed.add_field(name = bot.command_prefix + "갓겜판독기 게임이름", value = "특정게임이 갓겜인지 알려줍니다.", inline = False)
+    embed.add_field(name = bot.command_prefix + "병신판독기 이름", value = "특정 인물이 병신인지 알려줍니다.", inline = False)
+    embed.add_field(name = bot.command_prefix + "외화 외화이름", value = "특정 외화 시세를 알려줍니다(달러, 엔화, 위안화, 유로).", inline = False)
+    embed.add_field(name = bot.command_prefix + "메뉴추천 양식 / 한식 / 중식 / 일식 / 아무거나", value = "메뉴를 추천해줍니다", inline = False)
+    embed.add_field(name = bot.command_prefix + "롤전적검색 롤닉네임 ", value = "롤 전적 검색 기능입니다. 소환사 이름은 공백 없이 입력해주세요.", inline = False)
+    embed.add_field(name = bot.command_prefix + "롤체전적검색 롤닉네임 ", value = "롤체 전적 검색 기능입니다. 소환사 이름은 공백 없이 입력해주세요", inline = False)
+    embed.add_field(name = bot.command_prefix + "퇴근", value = "공붕이들을 위한 퇴근까지 남은 시간을 알려줍니다. 혹시 본인도 직장인이다 하면 알려주세요. 따로 퇴근시간 만들어 드립니다.", inline = False)
+    embed.add_field(name = bot.command_prefix + "계좌개설", value = "거래를 위한 계좌를 만듭니다. 기본 자금으로 3000만원을 받습니다.", inline = False)
+    embed.add_field(name = bot.command_prefix + "잔고조회", value = "내 계좌의 잔고를 조회합니다", inline = False)
+    embed.add_field(name = bot.command_prefix + "정보 @유저이름", value = "특정 유저의 잔고를 조회합니다", inline = False)
+    embed.add_field(name = bot.command_prefix + "송금 @유저이름", value = "현재 오류로 중지된 명령어", inline = False)
+    embed.add_field(name = bot.command_prefix + "외화매입/매각 외화이름", value = "추후 추가 예정인 명령어입니다", inline = False)
+    embed.add_field(name = bot.command_prefix + "주식매입/매각 주식이름", value = "추후 추가 예정인 명령어입니다", inline = False)
+    embed.add_field(name = bot.command_prefix + "코인매입/매각 코인이름", value = "추후 추가 예정인 명령어입니다", inline = False)
+    await ctx.send(embed=embed)
 
 @bot.command(name='탈모빔')
 async def cmd1(ctx, arg):
-    await ctx.send(hairlossbeam.hairlossbeam(arg))
+    await ctx.send(hairlossbeam(arg))
 
 
 @bot.command(name="갓겜판독기")
 async def cmd2(ctx,arg):
-    await ctx.send(godgame.godgame(arg))
+    await ctx.send(godgame(arg))
 
 
 @bot.command(name="병신판독기")
 async def cmd3(ctx,arg):
-    await ctx.send(sb.sb(arg))
+    await ctx.send(sb(arg))
 
 
 @bot.command(name='외화')
 async def cmd4(ctx, arg):
-    await ctx.send(money.foreignCurrency(arg))
+    await ctx.send(foreignCurrency(arg))
 
 
 @bot.command(name="메뉴추천")
 async def cmd5(ctx, arg):
-    return ctx.send(recommendfood.recommendfood(arg))
+    return ctx.send(recommendfood(arg))
 
 
 @bot.command(name='롤전적검색')
 async def cmd6(ctx, arg):
-    embed = search_lol_userInfo.search_lol_userInfo(arg, RIOT_TOKEN)
+    embed = search_lol_userInfo(arg, RIOT_TOKEN)
     await ctx.send(embed = embed)
 
 
 @bot.command(name= "롤체전적검색")
 async def cmd7(ctx, arg):
-    await ctx.send(embed = search_tft_userInfo.search_tft_userInfo(arg, RIOT_TOKEN))
+    await ctx.send(embed = search_tft_userInfo(arg, RIOT_TOKEN))
 
-@bot.command(name= "계좌개설")
+@bot.command(name="퇴근")
 async def cmd8(ctx):
-    user.signup(ctx.author.name, ctx.author.id)
-    await ctx.send(f"계좌 개설 완료")
+    await ctx.send(countTime())
+
+@bot.command(name="!p")
+async def cmExtra(ctx):
+    pass
+
+@bot.command()
+async def 계좌개설(ctx):
+    await ctx.send("계좌개설 가능여부를 조회합니다")
+    if findRow(ctx.author.name, ctx.author.id) == None:
+        signup(ctx.author.name, ctx.author.id)
+        await ctx.send("회원가입이 완료되었습니다.")
+    else:
+        await ctx.send("이미 가입하셨습니다. \n 중복 개설은 불가능합니다.")
+
+@bot.command()
+async def 잔고조회(ctx):
+    money = userInfo(ctx.author.name, ctx.author.id)
+    
+    if money == None:
+        print("사용자 정보가 없습니다.")
+        print("------------------------------\n")
+        await ctx.send("존재하지 않는 사용자")
+    else:
+        print("사용자 정보 발견, 메세지를 보냅니다.")
+        print("------------------------------\n")
+        embed = discord.Embed(title= ctx.author.name, color = 0x62D0F6)
+        embed.add_field(name = "잔고", value = money)
+        await ctx.send(embed=embed)
+
+@bot.command()
+async def 정보(ctx, user: discord.User):
+    money = userInfo(user.name, user.id)
+
+    if money == None:
+        print("사용자 정보가 없습니다.")
+        print("------------------------------\n")
+        await ctx.send("등록되지 않은 사용자입니다.")
+    else:
+        print("사용자 정보 발견, 메세지를 보냅니다.")
+        print("------------------------------\n")
+        embed = discord.Embed(title="유저 정보", description = user.name, color = 0x62D0F6)
+        embed.add_field(name = "보유 자산", value = money)
+        await ctx.send(embed=embed)
+
+@bot.command()
+async def 송금(ctx, user: discord.User, money):
+    if int(money) <= 0:
+        await ctx.send("송금 불가능 단위")
+        return 0
+        
+    print("송금이 가능한지 확인합니다.")
+    senderExistance, senderRow = checkUser(ctx.author.name, ctx.author.id)
+    receiverExistance, receiverRow = checkUser(user.name, user.id)
+
+    if not senderExistance:
+        print("DB에서", ctx.author.name, "을 찾을수 없습니다")
+        print("------------------------------\n")
+        await ctx.send("회원가입 후 송금이 가능합니다.")
+    elif not receiverExistance:
+        print("DB에서 ", user.name, "을 찾을 수 없습니다")
+        print("------------------------------\n")
+        await ctx.send(user.name  + " 은(는) 등록되지 않은 사용자입니다.")
+    else:
+        print("송금하려는 돈: ", money)
+
+        s_money = getMoney(ctx.author.name, senderRow)
+        r_money = getMoney(user.name, receiverRow)
+
+        if s_money >= int(money) and int(money) != 0:
+            print("돈이 충분하므로 송금을 진행합니다.")
+            print("")
+
+            remit(ctx.author.name, senderRow, user.name, receiverRow, money)
+
+            print("송금이 완료되었습니다. 결과를 전송합니다.")
+
+            embed = discord.Embed(title="송금 완료", description = "송금된 돈: " + money, color = 0x77ff00)
+            embed.add_field(name = "보낸 사람: " + ctx.author.name, value = "현재 자산: " + str(getMoney(ctx.author.name, senderRow)))
+            embed.add_field(name = "→", value = ":moneybag:")
+            embed.add_field(name="받은 사람: " + user.name, value="현재 자산: " + str(getMoney(user.name, receiverRow)))
+                    
+            await ctx.send(embed=embed)
+        elif int(money) == 0:
+            await ctx.send("0원을 보낼 필요는 없죠")
+        else:
+            print("돈이 충분하지 않습니다.")
+            print("송금하려는 돈: ", money)
+            print("현재 자산: ", s_money)
+            await ctx.send("돈이 충분하지 않습니다. 현재 자산: " + str(s_money))
+
+        print("------------------------------\n")
+
+@bot.command()
+async def reset(ctx):
+    delete()
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("명령어를 찾지 못했습니다")
 
 bot.run(DISCORD_TOKEN)
