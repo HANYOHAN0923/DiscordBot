54863f4798b404ee70ef164537d6a4c8e5aa8af4
HANYOHAN0923 <gksdygks2124@gmail.com>
2023-03-02 10:16:49 +0900

    add MusicPlayer

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from discord.ext import commands
 import discord
 import tomllib
-
+import youtube_dl
+from youtube_search import YoutubeSearch
+import asyncio
 
 # Custom Python Library
 from lib.money import *
@@ -942,6 +944,169 @@ async def 알까기(ctx):
     else:
         print("DB에서 조회할 수 없는 유저입니다.")
         await ctx.send("알까기는 회원가입 후 할 수 있습니다.")
+
+# ================================== Music Player ==================================
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+i = 0
+loop = False
+playlist = []
+
+def set_Embed(title = '', description = ''):
+    #embed = discord.Embed(title="메인 제목", description="설명", color=0x62c1cc)
+    return discord.Embed(title=title, description=description)
+
+
+async def song_start(voice, i):
+    try:
+        if not voice.is_playing() and not voice.is_paused():
+            ydl_opts = {'format': 'bestaudio'}
+            FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(f'https://www.youtube.com{playlist[i][1]}', download=False)
+                URL = info['formats'][0]['url']
+            
+            voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS))
+        #voice.play(discord.FFmpegPCMAudio(executable = './ffmpeg-4.4-full_build-shared/bin/ffmpeg.exe', source='./song.mp3'))
+
+        while voice.is_playing() or voice.is_paused():
+            await asyncio.sleep(0.1)
+    except:
+        return
+
+@bot.command(aliases = ['play', 'p', 'ㅔ'])
+async def Play(ctx,*,  keyword):
+    try:
+        results = YoutubeSearch(keyword, max_results=1).to_dict()
+
+        global playlist
+        playlist.append([results[0]['title'], results[0]['url_suffix']]) # 플레이리스트에 노래 추가
+        await ctx.send(embed=set_Embed(title='노래 추가',description=f"{results[0]['title']}"))
+
+        channel = ctx.author.voice.channel
+        if bot.voice_clients == []:
+            await channel.connect()
+            #await ctx.send("connected to the voice channel, " + str(bot.voice_clients[0].channel))
+        voice = bot.voice_clients[0]
+
+        if not voice.is_playing() and not voice.is_paused():
+            global i
+            i = 0
+            while True:
+                await song_start(voice, i)
+                if loop:
+                    if i < len(playlist) - 1:
+                        i = i + 1 
+                    else:
+                        i = 0
+                    continue
+                elif i < len(playlist) - 1:
+                    i = i + 1
+                    continue
+                playlist = [[]]
+                break
+        #await voice.disconnect()
+    except:
+        await ctx.send("Play Error")
+
+@bot.command(aliases = ['save'])
+async def Save(ctx, * , arg):
+    with open(f'{arg}.txt','w',encoding='UTF-8') as f:
+        global playlist
+        f.write(str(playlist))
+
+@bot.command(aliases = ['open'])
+async def Open(ctx, * , arg):
+    try:
+        with open(f'./{arg}.txt','r',encoding='UTF-8') as f:
+            global playlist
+            playlist = eval(f.read())
+
+        global i
+        await ctx.send(f"플레이리스트 추가 완료\n{arg}")
+
+        channel = ctx.author.voice.channel
+        if bot.voice_clients == []:
+            await channel.connect()
+
+        voice = bot.voice_clients[0]
+        if voice.is_playing() or voice.is_paused():
+            i = -1
+            bot.voice_clients[0].stop()
+        else:
+            i = 0
+            while True:
+                await song_start(voice, i)
+                if loop:
+                    if i < len(playlist) - 1:
+                        i = i + 1 
+                    else:
+                        i = 0
+                    continue
+                elif i < len(playlist) - 1:
+                    i = i + 1
+                    continue
+                playlist = [[]]
+                break
+    
+
+    except:
+        await ctx.send("Open Error")
+
+@bot.command(aliases = ['q', 'que'])
+async def Que(ctx):
+    try:
+        queText = ''
+        for title in range(len(playlist)):
+            try:
+                queText = f'{queText}\n' + f'{title + 1}. {playlist[title][0]}'
+            except:
+                del playlist[title]
+        await ctx.send(embed=set_Embed(title='플레이리스트',description=f"{queText}"))
+    except:
+        pass
+
+@bot.command(aliases = ['remove'])
+async def Remove(ctx, arg):
+    try:
+        global playlist
+        remove_song = playlist[int(arg)- 1][0]
+        del(playlist[int(arg)- 1])
+        global i
+        i = i - 1
+        if (i + 1) == int(arg) - 1:
+            bot.voice_clients[0].stop()
+        await ctx.send(embed=set_Embed(title='노래 삭제',description=f"{remove_song}"))
+    except:
+        await ctx.send('노래 제거중 오류 발생!')
+
+@bot.command(aliases = ['leave'])
+async def Leave(ctx):
+    try:
+        await bot.voice_clients[0].disconnect()
+    except:
+        await ctx.send('Leave Error')
+
+@bot.command(aliases = ['skip', 's'])
+async def Skip(ctx):
+    try:
+        bot.voice_clients[0].stop()
+    except:
+        await ctx.send("Skip Error")
+
+@bot.command(aliases = ['pause'])
+async def Pause(ctx):
+    try:
+        bot.voice_clients[0].pause()
+    except:
+        await ctx.send("Pause Error")
+
+@bot.command(aliases = ['resume'])
+async def Resume(ctx):
+    try:
+        bot.voice_clients[0].resume()
+    except:
+        await ctx.send("Resume Error")
 
 @bot.event
 async def on_command_error(ctx, error):
